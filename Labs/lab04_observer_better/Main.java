443f7988c089429dce788ade9d9d7a150e1578c8
lars1050 <larsonam@augsburg.edu>
2023-02-02 08:40:01 -0600

    posting observer better lab

--- Labs/lab04_observer_better/Main.java
+++ Labs/lab04_observer_better/Main.java
@@ -8,9 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // This is an observer of stock information (pushed out to a "database")
-        //DataFile db = new DataFile("dbtest.csv");
-
         // The subjects
         StockMarket stockMarket = new StockMarket();
         WeatherStation weatherStation = new WeatherStation();
@@ -21,56 +18,19 @@ public class Main {
         Newsfeed newsFeed = new Newsfeed(theNews,weatherStation,stockMarket);
         Database database = new Database(stockMarket);
 
+        // Subjects implement Runnable. Make Thread to run in.
         Thread stockThread = new Thread(stockMarket);
         Thread weatherThread = new Thread(weatherStation);
         Thread newsThread = new Thread(theNews);
 
+        // Start all threads
         stockThread.start();
         weatherThread.start();
         newsThread.start();
-        newsFeed.start();
+        
+        newsFeed.start();   // Subject thread that controls the scrolling headline
 
-        Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
         while (true) {
-            userInput = scanner.nextLine();
-            System.out.println("************************* "+userInput);
         }
-        //stockThread.interrupt();
-        //weatherThread.interrupt();
-        //newsThread.interrupt();
     }
 }
-
-
-        /* 
-
-        // This is "sort of" pushing out information to all the observers
-        long counter = 0;
-        for (long i=0; i<1000000; i++) {
-
-            // update the news
-            String temp = "news";
-            //temp += String.format("TEMP %d PREC %d%% AIR %d %n", temperature, precipitation, airQuality);
-            // += String.format("DOW %.2f S&P %.2f 3M %.2f Microsoft %.2f %n", dow, snp, mmm, msft);
-            if (counter%10 == 0) {
-                temp = "News item "+counter/10+" is some very interesting news!";
-                news.update(temp);
-            }
-            news.redraw();
-
-            // update the database
-            //db.addData("and another");
-         
-            // update the stock watcher 
-            //System.out.printf("DOW %.2f S&P %.2f 3M %.2f Microsoft %.2f %n", dow, snp, mmm, msft);
-
-            try {
-                Thread.sleep(200);
-                ++counter;
-            } catch (InterruptedException e) {
-            }
-        }
-    }    
-}
-*/
